7f6f88206980102a41dc20758bf9302dca61e25f
guimcaballero <guim.caballero@gmail.com>
2020-09-12 12:30:05 +0200

    Add changing camera

--- src/systems/camera.rs
+++ src/systems/camera.rs
@@ -1,5 +1,6 @@
 use crate::systems::unit::{TargetPosition, Unit};
 use bevy::{prelude::*, render::camera::Camera};
+use bevy_mod_picking::*;
 
 pub struct CanHaveCamera {
     translation_offset: Vec3,
@@ -42,12 +43,51 @@ fn update_camera_position(
 }
 
 fn reset_unit_target_if_it_has_camera(
-    mut query: Query<(&CanHaveCamera, &mut TargetPosition, &mut Unit)>,
+    // TODO Can this be a Changed?
+    mut query: Query<(&CanHaveCamera, &mut TargetPosition, &mut Unit, &mut Draw)>,
 ) {
-    for (can_have_camera, mut target, mut unit) in &mut query.iter() {
+    for (can_have_camera, mut target, mut unit, mut draw) in &mut query.iter() {
         if let Some(_) = can_have_camera.camera_entity {
             target.pos = None;
             unit.selected = false;
+            draw.is_visible = false;
+        } else {
+            draw.is_visible = true;
+        }
+    }
+}
+
+fn switch_camera_to_entity(
+    pick_state: Res<PickState>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mouse_button_inputs: Res<Input<MouseButton>>,
+    mut query: Query<(&mut CanHaveCamera, &Unit)>,
+) {
+    if !keyboard_input.pressed(KeyCode::M) || !mouse_button_inputs.just_pressed(MouseButton::Left) {
+        return;
+    }
+
+    // Get the entity selected
+    if let Some(top_pick) = pick_state.top() {
+        let camera_entity = {
+            let mut camera: Option<Entity> = None;
+            for (mut can_have_camera, _) in &mut query.iter() {
+                if can_have_camera.camera_entity.is_some() {
+                    camera = can_have_camera.camera_entity;
+                }
+                can_have_camera.camera_entity = None;
+            }
+            camera
+        };
+
+        let entity = top_pick.get_entity();
+        let unit_result = query.entity(entity);
+
+        if let Ok(mut unit) = unit_result {
+            if let Some((mut can_have_camera, _)) = unit.get() {
+                // This is the selected unit
+                can_have_camera.camera_entity = camera_entity;
+            }
         }
     }
 }
@@ -56,6 +96,7 @@ pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_system(update_camera_position.system())
+            .add_system(switch_camera_to_entity.system())
             .add_system(reset_unit_target_if_it_has_camera.system());
     }
 }
